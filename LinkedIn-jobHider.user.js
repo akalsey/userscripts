5ebe580df1a662111328849320ecf0fc41174bf7
Adam Kalsey <adam@kalsey.com>
2024-11-04 15:27:20 -0800

    Fix metadata

--- LinkedIn-jobHider.user.js
+++ LinkedIn-jobHider.user.js
@@ -2,9 +2,9 @@
 // @name        LinkedIn Job Hider
 // @version     1.0
 // @author      Adam Kalsey
-// @description Really hide job ads when you hit "Don't Show"
-// @include     http*://www.linkedin.com/
+// @description Really hide job ads when you hit Don't Show
+// @match       https://www.linkedin.com/*
 // @grant       GM_addStyle
-// ==/UserScript
+// ==/UserScript==
 
 GM_addStyle('.job-card-list--is-dismissed { display: none !important; }'); 
